e1c965fd6442771806eb73d78c2de63ca207ccd4
Psycho <ilya.efremov.0498@gmail.com>
2024-01-10 17:04:03 +0500

    Initial commit

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=50),
+    price = models.IntegerField(),
+    image = models.CharField(max_length=50),
+    release_date = models.CharField(max_length=50),
+    lte_exists = models.BooleanField(),
+    slug = models.SlugField(null=False, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs) - models
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +9,13 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phones_all = Phone.objects.all()
+    context = {'phone': phones_all}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.filter(slug__contains=slug).first()
+    context = {'phone': phone}
     return render(request, template, context)
